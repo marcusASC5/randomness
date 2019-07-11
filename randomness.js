02668a6e8f0d42585cb803df8c9d419a31fcd1d1
marcusASC5 <marcus.sutton29@yahoo.com>
2019-07-11 15:43:49 -0400

    Successful Presentation!

--- randomness.js
+++ randomness.js
@@ -3,21 +3,33 @@
 //////////////////////
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const foods = [
-    "pizza",
-    "burgers",
-    "rice and beans",
-    "lasagna",
-    "macaroni and cheese",
-    "baked potatoes"
+    // "pizza",
+    // "burgers",
+    // "rice and beans",
+    // "lasagna",
+    // "macaroni and cheese",
+    // "baked potatoes"
+    "beef patty",
+    "chicken",
+    "spaghetti",
+    "ice cream",
+    "french fries",
+    "sandwich"
 ];
 const students = [
-    "Bob",
-    "Martin",
-    "Danny",
-    "Mahdi",
-    "Harry",
-    "Sean",
-    "Robert"
+    // "Bob",
+    // "Martin",
+    // "Danny",
+    // "Mahdi",
+    // "Harry",
+    // "Sean",
+    // "Robert"
+    "Izzy",
+    "Joey",
+    "Aaron",
+    "Anthony",
+    "Max",
+    "Sam"
 ];
 /////////////////////////////
 // END OF GLOBAL VARIABLES //
@@ -44,8 +56,16 @@ function randomStudent(){
 randomStudent()
 
 function randomGroup(){
-    console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
-    console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
-    console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
+    let alpha1 = alphabet[Math.floor(Math.random() * alphabet.length)];
+    let alpha2 = alphabet[Math.floor(Math.random() * alphabet.length)];
+    let alpha3 = alphabet[Math.floor(Math.random() * alphabet.length)];
+    if(alpha1==alpha2 || alpha1==alpha3){
+        alpha1 = alphabet[Math.floor(Math.random() * alphabet.length)];
+    } else if(alpha2==alpha3){
+        alpha2 = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    console.log(alpha1);
+    console.log(alpha2);
+    console.log(alpha3);
 }
 randomGroup()
